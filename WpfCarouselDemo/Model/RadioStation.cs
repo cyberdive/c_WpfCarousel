42dddde795fba2aaa7ea784ff64e7dc22cd3e846
unknown <tellierp@stib.irisnet.be>
2019-11-06 13:02:33 +0100

    first init

---
+++ WpfCarouselDemo/Model/RadioStation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCarouselDemo.Model
+{
+    public class RadioStation
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string ImageSource { get; set; }
+        public string Text { get; set; }
+    }
+}
